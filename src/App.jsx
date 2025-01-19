86228a9234bff777a789c6d19abd48725c97a250
David Pérez <deividpa022@gmail.com>
2025-01-18 21:16:28 -0500

    feat: implement card matching logic and update game state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import cover from './assets/cover.png';
 import sword from './assets/sword-1.png';
@@ -10,17 +10,19 @@ import ring from './assets/ring-1.png';
 import Card from "./components/Card";
 
 const cardImages = [
-  { "src": sword, "alt": "Sword" },
-  { "src": shield, "alt": "Shield" },
-  { "src": scroll, "alt": "Scroll" },
-  { "src": potion, "alt": "Potion" },
-  { "src": helmet, "alt": "Helmet" },
-  { "src": ring, "alt": "Ring" }
+  { "src": sword, "alt": "Sword", matched: false },
+  { "src": shield, "alt": "Shield", matched: false },
+  { "src": scroll, "alt": "Scroll", matched: false },
+  { "src": potion, "alt": "Potion", matched: false },
+  { "src": helmet, "alt": "Helmet", matched: false },
+  { "src": ring, "alt": "Ring", matched: false }
 ]
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // Shuffle the cards
   const shuffleCards = () => {
@@ -32,17 +34,60 @@ function App() {
     setTurns(0);
   }
 
-  console.log(cards, turns)
+  // Handle a Choice
+  const handleChoice = (card) => {
+    if(choiceOne && choiceTwo) {
+      return;
+    }
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if(choiceOne.alt === choiceTwo.alt) {
+        if(choiceOne.id === choiceTwo.id) {
+          console.log("You can't pick the same card twice!")
+          return;
+        }
+        setCards(prevCards => prevCards.map((card) => {
+          if(card.alt === choiceOne.alt) {
+            return {...card, matched: true}
+          }
+          return card;
+        }))
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+
+      
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards);
+
+  // Reset choices and increment turns
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1);
+  }
 
   return (
     <>
       <main>
-        <h1>Magic Game</h1>
+        <h1>Memory Game</h1>
         <button onClick={shuffleCards}>New Game</button>
-
+        <p>Turns: {turns}</p>
         <div className="card-grid">
           {cards.map((card) => (
-            <Card key={card.id} src={card.src} alt={card.alt} cover={cover} />
+            <Card
+              key={card.id}
+              card={card}
+              cover={cover}
+              handleChoice={handleChoice}
+              flipped={card.id === choiceOne?.id || card.id === choiceTwo?.id || card.matched}
+            />
           ))}
         </div>
       </main>
