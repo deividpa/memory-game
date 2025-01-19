4ef00d2a0b71fcf667d84b1320ef21f7b6543b69
David Pérez <deividpa022@gmail.com>
2025-01-18 21:16:41 -0500

    feat: update Card component to handle click events and manage flipped state

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,32 @@
 import PropTypes from 'prop-types';
 
-const Card = ({src, alt, cover}) => {
+const Card = ({ card, cover, handleChoice, flipped }) => {
+
+  const { src, alt } = card;
+
+  const handleClick = () => {
+    console.log(alt, " flipped: ", flipped);
+    handleChoice(card);
+  }
+
   return (
     <div className="card">
-      <div>
-        <img className="front" src={src} alt={alt} />
-        <img className="back" src={cover} alt="Card Back" />
+      <div className={flipped ? "flipped" : ""}>
+          <img className="front" src={src} alt={alt} />
+          <img className="back" src={cover} alt="Card Back" onClick={handleClick} />
       </div>
     </div>
   )
 }
 
 Card.propTypes = {
-  id: PropTypes.number.isRequired,
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
+  card: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+  }).isRequired,
   cover: PropTypes.string.isRequired,
+  handleChoice: PropTypes.func.isRequired,
+  flipped: PropTypes.bool.isRequired,
 };
 
 export default Card;
